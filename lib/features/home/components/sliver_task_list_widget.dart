0138c616c438947a0f8de8ff292df64ff63295d4
Mohamedismaell <itsfacebooky@gmail.com>
2026-02-03 11:24:18 +0200

    provider for Home Screen

--- lib/features/home/components/sliver_task_list_widget.dart
+++ lib/features/home/components/sliver_task_list_widget.dart
@@ -1,54 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tasky/core/components/task_item_widget.dart';
-import 'package:tasky/models/task_model.dart';
+import 'package:tasky/features/home/controllers/home_controller.dart';
 
 class SliverTaskListWidget extends StatelessWidget {
   const SliverTaskListWidget({
     super.key,
-    required this.tasks,
-    required this.onTap,
-    required this.onDelete,
-    required this.onEdit,
-    this.emptyMessage,
   });
-
-  final List<TaskModel> tasks;
-  final Function(bool?, int?) onTap;
-  final Function(int?) onDelete;
-  final Function onEdit;
-  final String? emptyMessage;
-
+//CAn have message
   @override
   Widget build(BuildContext context) {
-    return tasks.isEmpty
-        ? SliverToBoxAdapter(
-            child: Center(
-              child: Text(
-                emptyMessage ?? 'No Data',
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
-            ),
-          )
-        : SliverPadding(
-            padding: EdgeInsets.only(bottom: 80),
-            sliver: SliverList.separated(
-              itemCount: tasks.length,
-              itemBuilder: (BuildContext context, int index) {
-                return TaskItemWidget(
-                  model: tasks[index],
-                  onChanged: (bool? value) {
-                    onTap(value, index);
+    return Consumer<HomeController>(
+        builder: (context, controller, child) => controller.tasks.isEmpty
+            ? SliverToBoxAdapter(
+                child: Center(
+                  child: Text(
+                    // controller.
+                    'No Data',
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              )
+            : SliverPadding(
+                padding: EdgeInsets.only(bottom: 80),
+                sliver: SliverList.separated(
+                  itemCount: controller.tasks.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return TaskItemWidget(
+                        model: controller.tasks[index],
+                        onChanged: (bool? value) {
+                          controller.doneTask(value, index);
+                        },
+                        onDelete: (int id) {
+                          controller.deleteTask(id);
+                        },
+                        onEdit: () => controller.loadTask);
                   },
-                  onDelete: (int id) {
-                    onDelete(id);
+                  separatorBuilder: (BuildContext context, int index) {
+                    return SizedBox(height: 8);
                   },
-                  onEdit: () => onEdit(),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return SizedBox(height: 8);
-              },
-            ),
-          );
+                ),
+              ));
   }
 }
