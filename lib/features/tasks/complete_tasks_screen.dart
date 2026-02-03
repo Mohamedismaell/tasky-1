6d85a036b766b1b72646360c926a97e392993bc8
Mohamedismaell <itsfacebooky@gmail.com>
2026-02-04 00:12:23 +0200

    manage controller for complete tasks screen

--- lib/features/tasks/complete_tasks_screen.dart
+++ lib/features/tasks/complete_tasks_screen.dart
@@ -1,116 +1,48 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:tasky/core/services/preferences_manager.dart';
-import 'package:tasky/models/task_model.dart';
+import 'package:provider/provider.dart';
 import 'package:tasky/core/components/task_list_widget.dart';
+import 'package:tasky/features/tasks/tasks_controller.dart';
 
-class CompleteTasksScreen extends StatefulWidget {
+class CompleteTasksScreen extends StatelessWidget {
   const CompleteTasksScreen({super.key});
 
   @override
-  State<CompleteTasksScreen> createState() => _CompleteTasksScreenState();
-}
-
-class _CompleteTasksScreenState extends State<CompleteTasksScreen> {
-  List<TaskModel> completeTasks = [];
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _loadTask();
-  }
-
-  void _loadTask() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    final finalTask = PreferencesManager().getString('tasks');
-    if (finalTask != null) {
-      final taskAfterDecode = jsonDecode(finalTask) as List<dynamic>;
-      setState(() {
-        completeTasks =
-            taskAfterDecode.map((element) => TaskModel.fromJson(element)).where((element) => element.isDone).toList();
-      });
-    }
-
-    setState(() {
-      isLoading = false;
-    });
-  }
-
-  _deleteTask(int? id) async {
-    List<TaskModel> tasks = [];
-    if (id == null) return;
-
-    final finalTask = PreferencesManager().getString('tasks');
-    if (finalTask != null) {
-      final taskAfterDecode = jsonDecode(finalTask) as List<dynamic>;
-      tasks = taskAfterDecode.map((element) => TaskModel.fromJson(element)).toList();
-      tasks.removeWhere((e) => e.id == id);
-
-      setState(() {
-        completeTasks.removeWhere((task) => task.id == id);
-      });
-
-      final updatedTask = tasks.map((element) => element.toJson()).toList();
-      PreferencesManager().setString('tasks', jsonEncode(updatedTask));
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Text(
-            'Completed Tasks',
-            style: Theme.of(context).textTheme.labelSmall,
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: isLoading
-                ? Center(
-                    child: CircularProgressIndicator(
-                    value: 20,
-                  ))
-                : TaskListWidget(
-                    tasks: completeTasks,
-                    onTap: (value, index) async {
-                      setState(() {
-                        completeTasks[index!].isDone = value ?? false;
-                      });
-
-                      final allData = PreferencesManager().getString('tasks');
-
-                      if (allData != null) {
-                        List<TaskModel> allDataList =
-                            (jsonDecode(allData) as List).map((element) => TaskModel.fromJson(element)).toList();
-                        final int newIndex = allDataList.indexWhere((e) => e.id == completeTasks[index!].id);
-                        allDataList[newIndex] = completeTasks[index!];
-
-                        await PreferencesManager().setString('tasks', jsonEncode(allDataList));
-                        _loadTask();
-                      }
-                    },
-                    emptyMessage: 'No Task Found',
-                    onDelete: (int? id) {
-                      _deleteTask(id);
-                    },
-                    onEdit: () {
-                      _loadTask();
-                    },
-                  ),
-          ),
-        ),
-      ],
+    return ChangeNotifierProvider<TasksController>(
+      create: (context) => TasksController()..init(),
+      child: Builder(builder: (context) {
+        final controller = context.read<TasksController>();
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text(
+                'Completed Tasks',
+                style: Theme.of(context).textTheme.labelSmall,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: controller.isLoading
+                    ? Center(
+                        child: CircularProgressIndicator(
+                        value: 20,
+                      ))
+                    : Consumer<TasksController>(
+                        builder: (context, value, _) => TaskListWidget(
+                            tasks: value.completeTasks,
+                            onTap: value.doneCompleteTask,
+                            emptyMessage: 'No Task Found',
+                            onDelete: value.deleteTask,
+                            onEdit: value.loadTask),
+                      ),
+              ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
